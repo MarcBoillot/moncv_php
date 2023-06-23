d44f0298824d068d1427ccd7ad27710bc5e514d3
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2023-06-23 11:04:23 +0200

    sauvegarde des donnees utilisateur

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
     <?php  
         $metaTitle = "contactez-moi";
         $metaDescription = "bienvenu sur ma page contact";
-       
+        //filtre post pour supprimer les caracteres speciaux 
         $nom = filter_input(INPUT_POST, 'nom');
         $prenom = filter_input(INPUT_POST, 'prenom');
         $mail = filter_input(INPUT_POST, 'email');
@@ -10,19 +10,12 @@
         $reason = filter_input(INPUT_POST, 'selection');
         $message = filter_input(INPUT_POST, 'message');
         $data_contact = ["$genre", "$nom", "$prenom", "$mail" ,"$reason" ,"$message" ,"$data_contact"];
-        
+        var_dump($name);
         date_default_timezone_set('Europe/Paris');
-        echo $date = date('Y-M-d_H-i-s');
-           
-        require('header.php');
-
-        var_dump($_SERVER['REQUEST_METHOD']); 
-        if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email']) && !empty($_POST['sexe']) && !empty($_POST['selection']) && !empty($_POST['message'])) {
-            file_put_contents("contact/contact_$date.txt", implode("\n", $data_contact) );  
-        }else{
-            
-            echo 'champs non-rempli';
-        }
+        echo $date = date('Y-M-d_H-i-s');  
+        require('header.php');              
+    //$firstname = 
+    //if (value du first name != nulle )
       
     ?>
 
@@ -51,44 +44,84 @@
                     <legend class="entete-contact">Me contacter</legend>
                     <div>
                         <fieldset class="genre">
+                            
                             <legend class="selection-genre">Sélectionner votre genre :</legend>
                             <label for="homme">M.</label>
-                            <input type="radio" name="sexe" value="homme" id="homme">
+                            <input type="radio" name="sexe" value=<?php echo $genre?> id="homme">
+                            
                             <label for="femme">Mme.</label>
-                            <input type="radio" name="sexe" value="femme" id="femme">
+                            <input type="radio" name="sexe" value=<?php echo $genre?> id="femme">
+
+                            <?php 
+                            if (empty($_POST['sexe'])){
+                                echo 'champs non rempli';
+                            }
+                            ?>
                         </fieldset><br>
                     </div>
                     <div>
                         <label for="nameinput">Votre nom</label>
-                        <input type="text" name="nom" id="nameinput" placeholder="Dupont">
+                        <input type="text" name="nom" id="nameinput" placeholder="Dupont" value=<?php echo $nom?>>
+                        <?php 
+                            if (empty($_POST['nom'])){
+                                echo 'champs non rempli';
+                            }
+                        ?>
                     </div><br>
                     <div>
                         <label for="nameinput">Votre prenom</label>
-                        <input type="text" name="prenom" id="nameinput" placeholder="Pierre">
+                        <input type="text" name="prenom" id="nameinput" placeholder="Pierre" value=<?php echo $prenom?>>
+                        <?php 
+                            if (empty($_POST['prenom'])){
+                                echo 'champs non rempli';
+                            }
+                        ?>
                     </div><br>
                     <div>
                         <label for="emailinput">Votre e-mail</label>
-                        <input type="email" name="email" id="emailinput" placeholder="exemple@mail.fr">
+                        <input type="email" name="email" id="emailinput" placeholder="exemple@mail.fr" value=<?php echo $mail ?>>
+                        <?php 
+                            if (empty($_POST['email'])){
+                                echo 'champs non rempli';
+                                var_dump($_SERVER['REQUEST_METHOD']);
+                            }
+                        ?>
                     </div><br>
                     <div>
                             <legend>Raison du contact</legend>
 
                             <label for="raison" >Proposition d'emploi</label>
                             <input type="radio" name="selection" value="proposition" id=proposition> 
-
+                        
                             <label for="raison" >Demande d'informations</label>
                             <input type="radio" name="selection" value="demande" id=Demande_info>  
-
+                           
                             <label for="raison">Prestation</label>
-                            <input type="radio" name="selection" value="prestation" id=prestation>                
+                            <input type="radio" name="selection" value="prestation" id=prestation> 
+                            <?php
+                                if (empty($_POST['selection'])){
+                                    echo '*champs non rempli*';}
+                            ?>
                     </div>
                     <div>
                         <label for="message"></label>
-                        <textarea name="message" id="message" cols="60" rows="20" placeholder="Ecrivez votre message" class="fieldtextarea"></textarea><br>
+                        <textarea name="message" id="message" cols="60" rows="20" placeholder="Ecrivez votre message" class="fieldtextarea" value=<?php echo $message ?>></textarea><br>
+                        <?php 
+                            if (empty($_POST['message'])){
+                                echo 'champs non rempli';
+                            }
+                        ?>
                     </div>
                     <div>
                         <input type="submit" value="Envoyer un mail" name="envoyer">
                         <input type="reset" name="buton" value="Annuler">
+                        <?php
+                        if (!empty($_POST['nom']) && !empty($_POST['prenom'])&& !empty($_POST['email'])&& !empty($_POST['sexe'])&&!empty($_POST['selection'])&&!empty($_POST['message'])){
+                            var_dump($_SERVER['REQUEST_METHOD']); 
+                            file_put_contents("contact/contact_$date.txt", implode("\n", $data_contact) );   
+                        }
+                        
+                        ?>
                     </div>
                 </fieldset>
             </div>
